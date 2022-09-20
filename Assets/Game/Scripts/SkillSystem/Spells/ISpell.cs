acbcce73a7d148b49c4d167009b6f8cb6ee326ec
RenderingCode\Zack <renderingdevcode@gmail.com>
2022-09-20 23:15:57 +0200

    Updated spell interface

--- Assets/Game/Scripts/SkillSystem/Spells/ISpell.cs
+++ Assets/Game/Scripts/SkillSystem/Spells/ISpell.cs
@@ -1,7 +1,9 @@
+using UnityEngine;
+
 namespace CosmosDefender
 {
-	public interface ISpell
-	{
-	    
-	}
+    public interface ISpell
+    {
+        void Cast(Transform spawnPoint, IReadOnlyCombatData combatData);
+    }
 }
