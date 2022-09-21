6d6e3f3f2cd125d2511e34f4c448f6a46ae5ed96
LouisbossEX <guillemllovera@hotmail.com>
2022-09-21 12:21:14 +0200

    Solved AttributesData compilation error

--- Assets/Game/Scripts/SkillSystem/PlayerAttributes/AttributesData.cs
+++ Assets/Game/Scripts/SkillSystem/PlayerAttributes/AttributesData.cs
@@ -12,16 +12,20 @@ namespace CosmosDefender
         private float speed;
         [SerializeField]
         private float maxHealth;
+        [SerializeField]
+        private float cooldownReduction;
 
         public float AttackDamage { get => attackDamage; set => attackDamage = value; }
         public float Speed { get => speed; set => speed = value; }
         public float MaxHealth { get => maxHealth; set => maxHealth = value; }
+        public float CooldownReduction { get => cooldownReduction; set => cooldownReduction = value; }
 
-        public AttributesData(float attackDamage, float speed, float maxHealth)
+        public AttributesData(float attackDamage, float speed, float maxHealth, float cooldownReduction)
         {
             this.attackDamage = attackDamage;
             this.speed = speed;
             this.maxHealth = maxHealth;
+            this.cooldownReduction = cooldownReduction;
         }
     }
 }
