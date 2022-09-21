4125a9efd3758e8a3edc0719b915ea9c645db2bd
LouisbossEX <guillemllovera@hotmail.com>
2022-09-21 18:50:03 +0200

    Spell interfaces updated

--- Assets/Game/Scripts/SkillSystem/Spells/Implementation/LaserSpell_Base.cs
+++ Assets/Game/Scripts/SkillSystem/Spells/Implementation/LaserSpell_Base.cs
@@ -5,13 +5,13 @@ namespace CosmosDefender
     [CreateAssetMenu(fileName = nameof(LaserSpell_Base), menuName = "CosmosDefender/" + nameof(LaserSpell_Base))]
     public class LaserSpell_Base : BaseSpell
     {
-        public override void Cast(Transform spawnPoint, IReadOnlyCombatData combatData)
+        public override void Cast(Transform spawnPoint, IReadOnlyOffensiveData combatData)
         {
             // This spell casts lasers around the player.
             
         }
 
-        public override void Cast(Transform spawnPoint, Vector3 forward, Quaternion rotation, IReadOnlyCombatData combatData)
+        public override void Cast(Transform spawnPoint, Vector3 forward, Quaternion rotation, IReadOnlyOffensiveData combatData)
         {
             var instance = Object.Instantiate(prefab, spawnPoint.position, rotation);
             instance.GetComponent<Bullet>().InstantiateBullet(spawnPoint, forward, rotation);
--- Assets/Game/Scripts/SkillSystem/Spells/Implementation/ThunderSpell.cs
+++ Assets/Game/Scripts/SkillSystem/Spells/Implementation/ThunderSpell.cs
@@ -8,12 +8,12 @@ namespace CosmosDefender
         [SerializeField]
         private int amountOfMissiles;
 
-        public override void Cast(Transform spawnPoint, IReadOnlyCombatData combatData)
+        public override void Cast(Transform spawnPoint, IReadOnlyOffensiveData combatData)
         {
             // 
         }
         
-        public override void Cast(Transform spawnPoint, Vector3 forward, Quaternion rotation, IReadOnlyCombatData combatData)
+        public override void Cast(Transform spawnPoint, Vector3 forward, Quaternion rotation, IReadOnlyOffensiveData combatData)
         {
             var projectile = Object.Instantiate(prefab, spawnPoint.position, rotation);
             projectile.GetComponent<Bullet>().InstantiateBullet(spawnPoint, forward, rotation);
