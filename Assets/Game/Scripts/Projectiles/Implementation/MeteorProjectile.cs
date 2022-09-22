288856bc265e2cd75ddf44fcf6cd4fe93ace9f59
Jordi Alba <44704875+jalba002@users.noreply.github.com>
2022-09-22 13:50:09 +0200

    Deal damage entry and Cosmos Defender classification

--- Assets/Game/Scripts/Projectiles/Implementation/MeteorProjectile.cs
+++ Assets/Game/Scripts/Projectiles/Implementation/MeteorProjectile.cs
@@ -1,40 +1,47 @@
 using UnityEngine;
 using UnityEngine.VFX;
 
-public class MeteorProjectile : BaseProjectile
+namespace CosmosDefender.Projectiles
 {
-    [SerializeField]
-    private VisualEffect vfx;
-
-    [SerializeField] private ParticleSystem particles;
-
-    [SerializeField] private MeshRenderer mrend;
-
-    public override void InitializeProjectile(Vector3 spawnPoint, Vector3 velocity)
+    public class MeteorProjectile : BaseProjectile
     {
-        base.InitializeProjectile(spawnPoint, velocity);
-        // Enable collider?
-        m_Rigidbody.velocity = velocity;
-    }
+        [SerializeField] private VisualEffect vfx;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log($"{other.gameObject.name}");
-        vfx.SendEvent("Play");
-        vfx.transform.parent = null;
-        Ray r = new Ray(transform.position, Vector3.down);
-        //Physics.Raycast(r, out RaycastHit hit, 5f, Int32.MaxValue);
-        //vfx.transform.position = hit.point;
-        vfx.transform.position = other.ClosestPoint(transform.position);
-        
-        mrend.enabled = false;
-        m_Rigidbody.velocity = Vector3.zero;
-        m_Rigidbody.useGravity = false;
-        particles.Stop();
-        particles.gameObject.transform.parent = null;
-        //this.gameObject.SetActive(false);
-        Destroy(vfx.gameObject, 10f);
-        Destroy(particles.gameObject, 3f);
-        Destroy(this.gameObject);
+        [SerializeField] private ParticleSystem particles;
+
+        [SerializeField] private MeshRenderer mrend;
+
+        public override void InitializeProjectile(Vector3 spawnPoint, Vector3 velocity)
+        {
+            base.InitializeProjectile(spawnPoint, velocity);
+            // Enable collider?
+            m_Rigidbody.velocity = velocity;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Debug.Log($"{other.gameObject.name}");
+            vfx.SendEvent("Play");
+            vfx.transform.parent = null;
+            Ray r = new Ray(transform.position, Vector3.down);
+            //Physics.Raycast(r, out RaycastHit hit, 5f, Int32.MaxValue);
+            //vfx.transform.position = hit.point;
+            vfx.transform.position = other.ClosestPoint(transform.position);
+
+            mrend.enabled = false;
+            m_Rigidbody.velocity = Vector3.zero;
+            m_Rigidbody.useGravity = false;
+            particles.Stop();
+            particles.gameObject.transform.parent = null;
+            //this.gameObject.SetActive(false);
+            Destroy(vfx.gameObject, 10f);
+            Destroy(particles.gameObject, 3f);
+
+            var a = AreaAttacksManager.SphereOverlap(this.gameObject.transform.position, 3f, true);
+            // TODO get the correct damage from the spells settings.
+            AreaAttacksManager.DealDamageToCollisions<IDamageable>(a, 10f);
+            
+            Destroy(this.gameObject);
+        }
     }
 }
