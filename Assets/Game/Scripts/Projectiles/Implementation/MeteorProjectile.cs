6e7c36d6757dddfa40e2b3a29445cfef71c487a8
Jordi Alba <44704875+jalba002@users.noreply.github.com>
2022-09-22 14:01:59 +0200

    Projectile reformed

--- Assets/Game/Scripts/Projectiles/Implementation/MeteorProjectile.cs
+++ Assets/Game/Scripts/Projectiles/Implementation/MeteorProjectile.cs
@@ -18,30 +18,61 @@ namespace CosmosDefender.Projectiles
             m_Rigidbody.velocity = velocity;
         }
 
-        private void OnTriggerEnter(Collider other)
+        protected override void UpdateVFX()
         {
-            Debug.Log($"{other.gameObject.name}");
             vfx.SendEvent("Play");
             vfx.transform.parent = null;
-            Ray r = new Ray(transform.position, Vector3.down);
-            //Physics.Raycast(r, out RaycastHit hit, 5f, Int32.MaxValue);
-            //vfx.transform.position = hit.point;
-            vfx.transform.position = other.ClosestPoint(transform.position);
+            vfx.transform.position = transform.position; //other.ClosestPoint(transform.position);
+            Destroy(vfx.gameObject, 10f);
+        }
 
+        protected override void UpdateRenderer()
+        {
             mrend.enabled = false;
+        }
+
+        protected override void UpdateRigidbody()
+        {
             m_Rigidbody.velocity = Vector3.zero;
             m_Rigidbody.useGravity = false;
+        }
+
+        protected override void UpdateParticles()
+        {
             particles.Stop();
             particles.gameObject.transform.parent = null;
-            //this.gameObject.SetActive(false);
-            Destroy(vfx.gameObject, 10f);
-            Destroy(particles.gameObject, 3f);
+            
+            // TODO set in time.
+            CronoScheduler.Instance.ScheduleForTime(3f, () =>
+            {
+                particles.gameObject.SetActive(false);
+            });
+            //particles.gameObject.SetActive(false);
+            // Destroy(particles.gameObject, 3f);
+        }
 
+        protected override void CastDamage()
+        {
             var a = AreaAttacksManager.SphereOverlap(this.gameObject.transform.position, 3f, true);
+            
             // TODO get the correct damage from the spells settings.
             AreaAttacksManager.DealDamageToCollisions<IDamageable>(a, 10f);
-            
+        }
+
+        protected override void FinishObject()
+        {
+            // TODO switch this if there's a pooler somehow.
             Destroy(this.gameObject);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            UpdateVFX();
+            UpdateRenderer();
+            UpdateRigidbody();
+            UpdateParticles();
+            CastDamage();
+            FinishObject();
         }
     }
 }
