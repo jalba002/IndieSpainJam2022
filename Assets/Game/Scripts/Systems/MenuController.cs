450be812afae24f2b3f85ac7a3220c183fe69528
RenderingCode\Zack <renderingdevcode@gmail.com>
2022-09-22 21:38:12 +0200

    Updated scenes

--- Assets/Game/Scripts/Systems/MenuController.cs
+++ Assets/Game/Scripts/Systems/MenuController.cs
@@ -5,4 +5,24 @@ public class MenuController : MonoBehaviour
 {
     [SerializeField]
     private PlayerAttributes playerAttributes;
+    [SerializeField]
+    private ShopModifiers shopModifiers;
+    [SerializeField]
+    private CanvasMenu[] menus;
+
+    private void Start()
+    {
+        ActivateMenu(menus[0]);
+    }
+
+    public void ActivateMenu(CanvasMenu menu)
+    {
+        foreach (var current in menus)
+        {
+            if (current == menu)
+                current.Show();
+            else
+                current.Hide();
+        }
+    }
 }
