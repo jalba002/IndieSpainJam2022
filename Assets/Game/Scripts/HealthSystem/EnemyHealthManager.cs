45e85a869d6d302bd7db7285705b827aa21f2e80
Jordi Alba <jordi.alba@telefonica.net>
2022-09-26 17:12:01 +0200

    Death

--- Assets/Game/Scripts/HealthSystem/EnemyHealthManager.cs
+++ Assets/Game/Scripts/HealthSystem/EnemyHealthManager.cs
@@ -2,6 +2,7 @@ using CosmosDefender;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class EnemyHealthManager : HealthManager
 {
@@ -16,6 +17,8 @@ public class EnemyHealthManager : HealthManager
 
     private EnemySpawner enemySpawner;
     private EnemyAI enemyAI;
+
+    [SerializeField] private VisualEffect prefab;
 
     private void Awake()
     {
@@ -40,6 +43,9 @@ public class EnemyHealthManager : HealthManager
         GameManager.Instance.ResourceManager.IncreaseResource(ResourceType.Goddess, data.StarResourceOnDeath);
         enemySpawner.DecreaseCurrentEnemyCounter();
         enemyAI.Death();
+        var a =Instantiate(prefab, transform.position, Quaternion.identity);
+        a.SetVector3("Size", new Vector3(5f, 20f, 5f));
+        Destroy(a.gameObject, 2f);
         Destroy(gameObject);
     }
 
