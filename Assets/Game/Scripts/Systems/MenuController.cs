eb1b8db9078d426fc8f1bf3fbcb6ed4c38d84224
RenderingCode\Zack <renderingdevcode@gmail.com>
2022-09-26 18:21:18 +0200

    Main menu

--- Assets/Game/Scripts/Systems/MenuController.cs
+++ Assets/Game/Scripts/Systems/MenuController.cs
@@ -10,12 +10,16 @@ public class MenuController : MonoBehaviour
     private ShopModifiers shopModifiers;
     [SerializeField]
     private string gameLevel;
+    [SerializeField]
+    private CosmosSpell basicAttack;
 
     public void LoadGame()
     {
         playerAttributes.RemoveAllSpells();
         playerAttributes.RemoveAllAttributeModifiers();
         playerAttributes.RemoveAllSpellModifiers();
+
+        playerAttributes.AddSpell(basicAttack);
         playerAttributes.AddAttributeModifiers(shopModifiers.GetAttributeModifiers());
         playerAttributes.AddSpellModifiers(shopModifiers.GetSpellModifiers());
         SceneManager.LoadScene(gameLevel);
