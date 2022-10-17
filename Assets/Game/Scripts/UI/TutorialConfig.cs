2a729ccf0fc899421fec671956dcbe57603e6ff8
Andrew <73665985+andrewraya@users.noreply.github.com>
2022-10-17 18:20:49 +0200

    Jordi guapo

--- Assets/Game/Scripts/UI/TutorialConfig.cs
+++ Assets/Game/Scripts/UI/TutorialConfig.cs
@@ -5,9 +5,11 @@ namespace CosmosDefender
     [CreateAssetMenu(fileName = nameof(TutorialConfig), menuName = "CosmosDefender/UI/" + nameof(TutorialConfig))]
     public class TutorialConfig : ScriptableObject
     {
+        public string tableCode = "Tutorial";
+
         public string title = "Default";
 
-        [TextArea(20, 60)] public string description = "";
+        public string description = "";
 
         public string dismiss =
             "<size=30><color=green>ESC</color></size> o <size=30 ><color=green>START</color></size> para continuar.";
